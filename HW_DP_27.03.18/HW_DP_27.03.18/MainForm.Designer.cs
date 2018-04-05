b2559662c9f3763bed560dcea0196fb79cd40553
AlisherB <alisher_b_95@mail.ru>
2018-04-05 16:59:23 +0600

    added HW_DP_27.03.18

---
+++ HW_DP_27.03.18/HW_DP_27.03.18/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace HW_DP_27._03._18
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.multiline_checkBox = new System.Windows.Forms.CheckBox();
+            this.vscroll_checkBox = new System.Windows.Forms.CheckBox();
+            this.hscroll_checkBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // multiline_checkBox
+            // 
+            this.multiline_checkBox.AutoSize = true;
+            this.multiline_checkBox.Location = new System.Drawing.Point(13, 13);
+            this.multiline_checkBox.Name = "multiline_checkBox";
+            this.multiline_checkBox.Size = new System.Drawing.Size(68, 17);
+            this.multiline_checkBox.TabIndex = 0;
+            this.multiline_checkBox.Text = "MultiLine";
+            this.multiline_checkBox.UseVisualStyleBackColor = true;
+            this.multiline_checkBox.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // vscroll_checkBox
+            // 
+            this.vscroll_checkBox.AutoSize = true;
+            this.vscroll_checkBox.Location = new System.Drawing.Point(13, 54);
+            this.vscroll_checkBox.Name = "vscroll_checkBox";
+            this.vscroll_checkBox.Size = new System.Drawing.Size(59, 17);
+            this.vscroll_checkBox.TabIndex = 1;
+            this.vscroll_checkBox.Text = "VScroll";
+            this.vscroll_checkBox.UseVisualStyleBackColor = true;
+            this.vscroll_checkBox.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // hscroll_checkBox
+            // 
+            this.hscroll_checkBox.AutoSize = true;
+            this.hscroll_checkBox.Location = new System.Drawing.Point(13, 95);
+            this.hscroll_checkBox.Name = "hscroll_checkBox";
+            this.hscroll_checkBox.Size = new System.Drawing.Size(60, 17);
+            this.hscroll_checkBox.TabIndex = 2;
+            this.hscroll_checkBox.Text = "HScroll";
+            this.hscroll_checkBox.UseVisualStyleBackColor = true;
+            this.hscroll_checkBox.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 131);
+            this.Controls.Add(this.hscroll_checkBox);
+            this.Controls.Add(this.vscroll_checkBox);
+            this.Controls.Add(this.multiline_checkBox);
+            this.MaximumSize = new System.Drawing.Size(380, 170);
+            this.MinimumSize = new System.Drawing.Size(380, 170);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox multiline_checkBox;
+        private System.Windows.Forms.CheckBox vscroll_checkBox;
+        private System.Windows.Forms.CheckBox hscroll_checkBox;
+    }
+}
+
---
+++ HW_DP_27.03.18/HW_DP_27.03.18/MainForm.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HW_DP_27._03._18
+{
+    public partial class MainForm : Form
+    {
+        TextBox t;
+        public MainForm()
+        {
+            InitializeComponent();
+            t = new MyTextBox();
+            Controls.Add(t);
+        }
+
+        private void CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            if (checkBox.Checked)
+            {
+                Controls.Remove(t);
+                if (checkBox.Name == "multiline_checkBox")
+                {
+                    t = new Multiline(t);
+                }
+                else if (checkBox.Name == "hscroll_checkBox")
+                {
+                    t = new HScroll_TextBox(t);
+                }
+                else if (checkBox.Name == "vscroll_checkBox")
+                {
+                    t = new VScroll_TextBox(t);
+                }
+                Controls.Add(t);
+            }
+            else
+            {
+                Controls.Remove(t);
+                t = new MyTextBox(t);
+                if (multiline_checkBox.Checked)
+                {
+                    t = new Multiline(t);
+                }
+                else if (hscroll_checkBox.Checked)
+                {
+                    t = new HScroll_TextBox(t);
+                }
+                else if (vscroll_checkBox.Checked)
+                {
+                    t = new VScroll_TextBox(t);
+                }
+                Controls.Add(t);
+            }
+        }
+    }
+    public class MyTextBox : TextBox
+    {
+        public MyTextBox() : base()
+        {
+            Location = new Point(103, 13);
+            Size = new Size(249, 20);
+            Multiline = false;
+            ScrollBars = ScrollBars.None;
+            WordWrap = true;
+        }
+        public MyTextBox(TextBox t) : this()
+        {
+            Text = t.Text;
+            Location = t.Location;
+            Size = t.Size;
+        }
+    }
+    public class Multiline : MyTextBox
+    {
+        public Multiline(TextBox t) : base()
+        {
+            Text = t.Text;
+            Multiline = true;
+            ScrollBars = t.ScrollBars;
+            Size = new Size(249, 100);
+            Location = t.Location;
+        }
+    }
+    public class VScroll_TextBox : MyTextBox
+    {
+        public VScroll_TextBox(TextBox t) : base()
+        {
+            if (t.ScrollBars == ScrollBars.Both)
+            {
+                ScrollBars = t.ScrollBars;
+                Multiline = true;
+                Size = new Size(249, 100);
+            }
+
+
+            if (t.ScrollBars == ScrollBars.Horizontal)
+            {
+                ScrollBars = ScrollBars.Both;
+                Multiline = true;
+                Size = new Size(249, 100);
+            }
+            else
+            {
+                ScrollBars = ScrollBars.Vertical;
+                Multiline = true;
+                Size = new Size(249, 100);
+            }
+            
+            Text = t.Text;
+            Location = t.Location;
+        }
+    }
+    public class HScroll_TextBox : MyTextBox
+    {
+        public HScroll_TextBox(TextBox t) : base()
+        {
+            if (t.ScrollBars == ScrollBars.Both)
+            {
+                ScrollBars = t.ScrollBars;
+                Multiline = true;
+                Size = new Size(249, 100);
+            }
+                
+            if (t.ScrollBars == ScrollBars.Vertical)
+            {
+                WordWrap = false;
+                ScrollBars = ScrollBars.Both;
+                Multiline = true;
+                Size = new Size(249, 100);
+            }    
+            else
+            {
+                WordWrap = false;
+                ScrollBars = ScrollBars.Horizontal;
+                Multiline = true;
+                Size = new Size(249, 40);
+            }
+
+            Text = t.Text;
+            Location = t.Location;
+        }
+    }
+}
